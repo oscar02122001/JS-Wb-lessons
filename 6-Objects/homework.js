f0e7fe526cae35b2329e1cf85803c2aa2b2be2e3
oscar02122001 <olimjonismatov01@gmail.com>
2023-02-06 23:56:24 +0500

    'objects_exercises'

---
+++ 6-Objects/homework.js
@@ -0,0 +1,37 @@
+//Export module
+
+// //1
+// const user = {}
+// //2
+// user.name = 'Jonas'
+// //3
+// user.surname = 'Smith'
+// //4
+// user.name = 'Pete'
+// //5
+// delete user.name
+
+
+// let salaries = {
+//     John: 100,
+//     Ann: 160,
+//     Pete: 130
+// }
+
+// const aboutSalaries = function (salaries) {
+//     let sum = 0;
+//     let maxSalery = 0;
+//     let whoIsMax = null;
+
+//     for (const [name, salary] of Object.entries(salaries)) {
+//         if (maxSalery < salary) {
+//             maxSalery = salary
+//             whoIsMax = name
+//         }
+//         sum += salary
+//     }
+//     console.log(`Total salary is ${sum} and ${whoIsMax} is earns (${maxSalery}) more than others`);
+// }
+// aboutSalaries(salaries)
+
+
