fc77ffa6765abe518d3a31292d25329848af9617
oscar02122001 <olimjonismatov01@gmail.com>
2023-02-07 17:36:02 +0500

    'object_exercises'

--- 6-Objects/homework.js
+++ 6-Objects/homework.js
@@ -35,3 +35,31 @@
 // aboutSalaries(salaries)
 
 
+// let menu = {
+//     width: 200,
+//     height: 300,
+//     title: 'My menu'
+// }
+
+
+// const multiplyNumeric = function (obj) {
+//     for (let [key, value] of Object.entries(obj)) {
+//         if (typeof value === 'number') {
+//             console.log(key, value * 2)
+//         }
+//     }
+// }
+// multiplyNumeric(menu)
+
+
+// function makeUser() {
+//     return {
+//         name: 'John',
+//         ref: this
+//     }
+// }
+
+// let user = makeUser()
+// alert(user.ref.name); 
+////  Error chiqaradi chunki ref:this undefined bogani uchun undefined qiymatdan name veribelini oqimaydi
+
--- 6-Objects/main.js
+++ 6-Objects/main.js
@@ -1,1 +1,22 @@
 import './homework.js'
+
+//--------6.1---------//
+
+// - What is Objects
+// - Create Objects ways
+// - adding keys and value and delete those
+// - types of obtaining information from the Object
+// - dynamic key obj[...]
+// - abbreviation with keys and veriable
+// - .... in obj 
+// - for loop with in and get key and value from Object
+// - Object.assign(...) , structuredClone, spread operator
+
+
+//--------6.2---------//
+
+// - Gaebage collection
+// - Object methods - function
+// - THIS
+// - obtional chaining
+// - destructure in objects
