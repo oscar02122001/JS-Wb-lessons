ee9362e1511a8490f5ffdfaeca41691b399a0d06
oscar02122001 <olimjonismatov01@gmail.com>
2023-03-22 15:48:20 +0500

    classes basic

--- 23-Classes Basic/homework.js
+++ 23-Classes Basic/homework.js
@@ -1,9 +1,10 @@
 class Rabbit extends Object {
     constructor(name) {
-       // Error is here: (Supper() to inherit from parent)
+        // Error is here: (Supper() to inherit from parent)
+        super()
         this.name = name
     }
 }
 
 let rabbit = new Rabbit("Rab")
-rabbit.hasOwnProperty('name')   
+rabbit.hasOwnProperty('name')
