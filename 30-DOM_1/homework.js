0a6eaad948dc83c43f7317b04cab67f7f5fbcf2d
oscar02122001 <olimjonismatov01@gmail.com>
2023-04-02 07:55:37 +0500

    Drop dawn

--- 30-DOM_1/homework.js
+++ 30-DOM_1/homework.js
@@ -1,7 +1,8 @@
 /// remove ball
-
 const table = document.querySelector('#table')
 const ball = document.querySelector('.ball')
+const tData = document.getElementsByTagName('td')
+
 
 let dot
 table.addEventListener('click', (e) => {
@@ -9,6 +10,47 @@ table.addEventListener('click', (e) => {
         dot.classList.remove('ball')
     }
     dot = e.target;
-    dot.classList.add('ball')
+    if (dot.innerHTML == '.') {
+        dot.classList.add('ball')
+    } else {
+        [...tData].forEach(val => val.classList.add('errClick'))
+        setTimeout(() => {
+            [...tData].forEach(val => val.classList.remove('errClick'))
+        }, 400)
+    }
 })
 
+
+/// FAQ
+const show = document.querySelector('.outerUl')
+const img = document.querySelector('.imgD')
+
+show.onclick = () => {
+    img.classList.toggle('active')
+    show.classList.toggle('show')
+}
+
+// Comments
+
+let item = document.querySelectorAll('.itemCom')
+let list = document.querySelector('.comments__list')
+// let arrItem = [...item]
+
+// let delet = function (el) {
+//     arrItem = arrItem.filter(val => val !== el);
+//     console.log(arrItem);
+//     getData()
+// }
+
+let getData = function () {
+    item.forEach(i => {
+        let button = document.createElement('button')
+        // button.addEventListener('click', () => delet(i))
+        button.classList.add('btn')
+        i.prepend(button)
+        button.onclick = () => i.remove()
+
+
+    })
+}
+getData()
